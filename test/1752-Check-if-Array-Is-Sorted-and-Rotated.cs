584767c7cd5a2fcece95301b9364e35551571e82
Amr shamakh <138307951+amrshamakh@users.noreply.github.com>
2025-02-02 10:35:05 +0200

    [1752] [Time Beats: 100.00%] [Memory Beats: 53.11%] - LeetPush

--- test/1752-Check-if-Array-Is-Sorted-and-Rotated.cs
+++ test/1752-Check-if-Array-Is-Sorted-and-Rotated.cs
@@ -1,30 +1,11 @@
 public class Solution {
-     public static bool IsSorted(int[] arr)
-        {
-            if (arr == null) return true;
-            for (int i = 0; i < arr.Length-1; i++) {
-                if (arr[i + 1] < arr[i]) return false;
-            }
-            return true;
-        }
-        public  bool Check(int[] nums)
-        {
-            int[]A=new int[nums.Length];
-            int x = 0;
-            while (x < nums.Length)
-            {
-                for (int i = 0; i < nums.Length; i++)
-                {
-                    A[(i + x) % nums.Length] = nums[i];
-                }
-
-                if (IsSorted(A)){
-                    Console.WriteLine(x);
-                    return true; 
-                }
-                x++;
-            }
-            return false;
-        }
+    public bool Check(int[] nums) {
+       int count=1;
+       for(int i=1;i<2*nums.Length;i++){
+        if(nums[i%nums.Length]>=nums[(i-1)%nums.Length])count++;
+        else count=1;
+        if(count==nums.Length)return true;
+       } 
+       return nums.Length==1;
     }
-
+}
