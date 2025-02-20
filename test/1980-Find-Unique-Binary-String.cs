e91d90ecca9cc97c5317bf92e14d530a913349e1
Amr shamakh <138307951+amrshamakh@users.noreply.github.com>
2025-02-20 14:34:18 +0200

    [1980] [Time Beats: 100.00%] [Memory Beats: 57.69%] - LeetPush

--- test/1980-Find-Unique-Binary-String.cs
+++ test/1980-Find-Unique-Binary-String.cs
@@ -1,17 +1,23 @@
 public class Solution {
     public string FindDifferentBinaryString(string[] nums) {
-        int n=nums[0].Length;
-        int maxV=(int)Math.Pow(2,n)-1;
-        HashSet<int> myHash=new HashSet<int>();
-        for(int i=0;i<nums.Length;i++){
-            myHash.Add(Convert.ToInt32(nums[i], 2));
-        }
-        for(int i=0;i<=maxV;i++){
-            if(!myHash.Contains(i))
-             return Convert.ToString(i, 2).PadLeft(n, '0');
+        // int n=nums[0].Length;
+        // int maxV=(int)Math.Pow(2,n)-1;
+        // HashSet<int> myHash=new HashSet<int>();
+        // for(int i=0;i<nums.Length;i++){
+        //     myHash.Add(Convert.ToInt32(nums[i], 2));
+        // }
+        // for(int i=0;i<=maxV;i++){
+        //     if(!myHash.Contains(i))
+        //      return Convert.ToString(i, 2).PadLeft(n, '0');
             
+        // }
+        // return null;
+        StringBuilder result=new StringBuilder();
+        for(int i=0;i<nums.Length;i++){
+            result.Append(nums[i][i]=='0'?'1':'0');
         }
-        return null;
+        return result.ToString();
+
         
     }
 }
