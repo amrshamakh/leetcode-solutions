8f83ad0757685e8164daaad6d42b4b0f173b7137
Amr shamakh <138307951+amrshamakh@users.noreply.github.com>
2025-02-21 18:49:41 +0200

    [338] [Time Beats: 100.00%] [Memory Beats: 68.76%] - LeetPush

--- test/338-Counting-Bits.cs
+++ test/338-Counting-Bits.cs
@@ -1,13 +1,12 @@
 public class Solution {
     public int[] CountBits(int n) {
         int[]result=new int[n+1];
-        result[0]=0;
         for(int i=1;i<=n;i++){
-          int  currentNum=i;
-            while(currentNum>0){
-                if(currentNum%2==1)result[i]++;
-                currentNum/=2;
-            }
-    }
+            result[i]=result[i/2]+(i&1);
+        }
     return result;
-}}
+
+    }
+}
+
+
