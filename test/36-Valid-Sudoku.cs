ffb8603750ad7a743d97fbb2d60401114c43f038
Amr shamakh <138307951+amrshamakh@users.noreply.github.com>
2025-02-25 17:48:23 +0200

    [36] [Time Beats: 87.09%] [Memory Beats: 80.48%] - LeetPush

--- test/36-Valid-Sudoku.cs
+++ test/36-Valid-Sudoku.cs
@@ -1,8 +1,9 @@
 public class Solution {
     public bool IsValidSudoku(char[][] board) {
+        HashSet<char> set=new HashSet<char>();
         // Each row must contain the digits 1-9 without repetition
         for(int i=0;i<board.Length;i++){
-        HashSet<char> set=new HashSet<char>();
+        set.Clear();
             for(int j=0;j<9;j++){
                 if(board[i][j]=='.')continue;
                 if(set.Contains(board[i][j]))return false;
@@ -11,7 +12,7 @@ public class Solution {
             }
             // Each column must contain the digits 1-9 without repetition
      for(int j=0;j<9;j++){
-        HashSet<char> set=new HashSet<char>();
+               set.Clear();
             for(int i=0;i<9;i++){
                 if(board[i][j]=='.')continue;
                 if(set.Contains(board[i][j]))return false;
@@ -21,7 +22,7 @@ public class Solution {
         // Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
         for(int row=0;row<9;row+=3){
             for(int col=0;col<9;col+=3){
-        HashSet<char> set=new HashSet<char>();
+                set.Clear();
                 for(int i=0;i<3;i++ ){
                     for(int j=0;j<3;j++){
                         char value=board[row+i][col+j];
