0e88ff608d022e9657716315e73623c78c4a8eef
Amr shamakh <138307951+amrshamakh@users.noreply.github.com>
2025-03-09 20:08:34 +0200

    [3208] [Time Beats: 92.31%] [Memory Beats: 100.00%] - LeetPush

--- test/Alternating-Groups-II.cs
+++ test/Alternating-Groups-II.cs
@@ -1,21 +1,12 @@
 public class Solution {
     public int NumberOfAlternatingGroups(int[] colors, int k) {
         int n=colors.Length;
-        int sum=0;
-        int[]valid=new int[n];
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(colors[i]!=colors[(i+1)%n])valid[i]=1;
+        int result=0,count=1;
+        for(int i=0;i<n+k-2;i++){
+            if(colors[i%n]!=colors[(i+1)%n])count++;
+            else count=1;
+            if(count>=k)result++;
         }
-        for(int i=0;i<k-1;i++){
-            sum+=valid[i];
-        }
-      
-        for(int i=0;i<n;i++){
-           if(sum==k-1)count++;
-           sum-=valid[i];
-           sum+=valid[(i+k-1)%n];
-        }
-        return count;
+    return result;
     }
 }
