91a4b49fd20c3e505b8e4919077a3aafa2b93547
Amr shamakh <138307951+amrshamakh@users.noreply.github.com>
2025-04-20 12:42:07 +0200

    [2733] [Time Beats: 100.00%] [Memory Beats: 59.38%] - LeetPush

--- test/Neither-Minimum-nor-Maximum.cs
+++ test/Neither-Minimum-nor-Maximum.cs
@@ -7,16 +7,22 @@ public class Solution {
       loop over nums and get the num is not equal them
       */ 
       
-      if(nums.Length<3)return -1; 
-      int max=int.MinValue,min=int.MaxValue;
-      for(int i=0;i<nums.Length;i++){
-        if(nums[i]>max)max=nums[i];
-        if(nums[i]<min)min=nums[i];
-      }
-      for(int i=0;i<nums.Length;i++){
-        if(nums[i]!=min&&nums[i]!=max)return nums[i];
-      }
-      return -1;
-
+    //   if(nums.Length<3)return -1; 
+    //   int max=int.MinValue,min=int.MaxValue;
+    //   for(int i=0;i<nums.Length;i++){
+    //     if(nums[i]>max)max=nums[i];
+    //     if(nums[i]<min)min=nums[i];
+    //   }
+    //   for(int i=0;i<nums.Length;i++){
+    //     if(nums[i]!=min&&nums[i]!=max)return nums[i];
+    //   }
+    //   return -1;
+    //what if we take 3 elements and return the number is valid of them 
+    if(nums.Length<3)return -1; 
+    int min=Math.Min(nums[0],nums[2]);
+    int max=Math.Max(nums[0],nums[2]);
+    if(nums[1]<max&&nums[1]>min)return nums[1];
+    else if(nums[1]<min)return min;
+    else return max;
     }
 }
