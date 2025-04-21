f30f5c051bc60ab7033265bcad0b3bfac8deb34a
Amr shamakh <138307951+amrshamakh@users.noreply.github.com>
2025-04-21 13:34:26 +0200

    [2124] [Time Beats: 100.00%] [Memory Beats: 76.00%] - LeetPush

---
+++ test/Check-if-All-A's-Appears-Before-All-B's.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public bool CheckString(string s) {
+        for(int i=1;i<s.Length;i++){
+            if(s[i]=='a'&&s[i-1]=='b')return false ;
+        }
+        return true;
+    }
+}
