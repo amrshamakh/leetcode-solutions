52fe2ffd84c9d6278eaf28d744252783c89ad619
Amr shamakh <138307951+amrshamakh@users.noreply.github.com>
2025-04-21 14:03:02 +0200

    [3222] [Time Beats: 100.00%] [Memory Beats: 65.38%] - LeetPush

--- test/Find-the-Winning-Player-in-Coin-Game.cs
+++ test/Find-the-Winning-Player-in-Coin-Game.cs
@@ -1,14 +1,18 @@
 public class Solution {
     public string WinningPlayer(int x, int y) {
         /*every time it should be x>0 and y >4*/
-        int f=1;
-        while(x>=1&&y>=4)
-        {
-            x-=1;
-            y-=4;
-            f++;
-        }
-        if(f%2==1)return "Bob";
-        else return "Alice";
+        // int f=1;
+        // while(x>=1&&y>=4)
+        // {
+        //     x-=1;
+        //     y-=4;
+        //     f++;
+        // }
+        // if(f%2==1)return "Bob";
+        // else return "Alice";
+
+        int turn= Math.Min(x,y/4);
+        return turn%2==1 ? "Alice":"Bob";
+        
     }
 }
