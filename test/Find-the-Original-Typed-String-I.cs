d723a83d2918d9c0c59c132d3dac7185805387e4
Amr shamakh <138307951+amrshamakh@users.noreply.github.com>
2025-04-22 13:07:27 +0200

    [3330] [Time Beats: 64.00%] [Memory Beats: 28.00%] - LeetPush

---
+++ test/Find-the-Original-Typed-String-I.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int PossibleStringCount(string word) {
+        int count=1;
+        for(int i=1;i<word.Length;i++){
+            if(word[i]==word[i-1])count++;
+        }
+        return count;
+    }
+}
